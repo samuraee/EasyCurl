2d82a44cac621000c5d80b3776d5b1f4a3b732aa
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2021-04-02 17:37:53 +0430

    fixed previous exception in EasyCurl exception

--- src/Exception.php
+++ src/Exception.php
@@ -16,6 +16,6 @@ class Exception extends \Exception
      */
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
-        parent::__construct($message, $code, Throwable) ;
+        parent::__construct($message, $code, $previous) ;
     }
 }
